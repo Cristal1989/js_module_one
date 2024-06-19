0f6e95266323a40a90bc700346d8226f9f2f3681
Cristal1989 <gips5921@gmail.com>
2024-06-19 14:57:25 +0300

    1

--- js/moduleFour.js
+++ js/moduleFour.js
@@ -118,3 +118,118 @@ repeat(3, printValue); //0-2
 
 // Передадим prettyPrint как callback функцию
 repeat(3, prettyValue); //Logging value: 0-2
+
+// ============================= forEach() ==========================
+
+const numbers = [2, 5, 7, 4, 1, 0];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(`Индекс ${i}, значение ${numbers[i]}`);
+}
+
+// Метод перебора через forEach()
+numbers.forEach(function (number, index) {
+  console.log(`Индекс ${index}, значение ${number}`);
+});
+
+// ======================== Cтрелочные функции ====================
+
+function multiply(a, b, c) {
+  return a + b + c;
+  // Это обычная функция
+}
+
+const foo = (a, b, c) => {
+  return a + b + c;
+  // Это стрелочная функция
+};
+
+// ============================= Неявное возвращение ==============
+
+const name = (a, b, c) => {
+  console.log(a, b, c);
+  return a + b + c;
+};
+
+const name = (a, b, c) => a + b + c;
+console.log(name(3, 4, 5)); //12
+
+// ============================= Псевдомассив arguments ========================
+
+const fn = (...args) => {
+  console.log(args);
+};
+
+fn(3, 2, 1, 0); //[ 3, 2, 1, 0 ]
+
+// =========================== Стрелочные функции как callback ==================
+
+const array = [3, 5, 8, 9, 10];
+array.forEach(function (number, index) {
+  console.log(`Индекс ${index}, значение ${number}`);
+});
+
+// Анонимная стрелочная функция
+array.forEach((number, index) => {
+  console.log(`Индекс ${index}, значение ${number}`);
+});
+
+// =================
+
+const array = [3, 5, 8, 9, 10];
+const logMessage = (number, index) => {
+  console.log(`Индекс ${index}, значение ${number}`);
+};
+
+array.forEach(logMessage);
+
+// =============================== Императивное программирование ============
+
+// Императивный подход
+const array = [3, 5, 8, 9, 10];
+const filterArray = [];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 7) {
+    filterArray.push(array[i]);
+  }
+}
+
+console.log(filterArray);
+
+// Декларативный подход
+const array = [3, 5, 8, 9, 10];
+const filterArray = array.filter(num => num > 7);
+
+console.log(filterArray);
+
+// ===================================== Функция с побочными эффектами =================
+
+const multiply = (array, value) => {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i] * value;
+  }
+};
+const numArr = [4, 7, 1, 4, 10, 22, 50];
+
+multiply(numArr, 2);
+// Мутация исходных данных - массива numArr
+console.log(numArr); //[ 8, 14, 2, 8, 20, 44, 100]
+
+// =========================== Чистая функция =====================
+
+const multiply = (arr, value) => {
+  let newArr = [];
+  arr.forEach(element => {
+    newArr.push(element * value);
+  });
+  return newArr;
+};
+
+const numArr = [4, 7, 1, 4, 10, 22, 50];
+const doubledArr = multiply(numArr, 2);
+
+// Мутация исходных данных не состоялась
+console.log(numArr); //[4, 7, 1, 4, 10, 22, 50]
+// Функция вернула новый массив с измененными данными
+console.log(doubledArr); //[ 8, 14, 2, 8, 20, 44, 100]
